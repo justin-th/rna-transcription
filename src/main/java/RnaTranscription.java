b05294593fa8f5504a71cdda032bc48667c73aa9
Justin Thomas <justinthomas@outlook.in>
2019-08-06 02:08:02 +0530

    Added Solution

--- src/main/java/RnaTranscription.java
+++ src/main/java/RnaTranscription.java
@@ -1,7 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class RnaTranscription {
 
+    // Create and populate constant transcriptions
+    public static final Map<Character, Character> transcriptions = new HashMap<>();
+    static {
+        transcriptions.put('G', 'C');
+        transcriptions.put('C', 'G');
+        transcriptions.put('T', 'A');
+        transcriptions.put('A', 'U');
+    }
+
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder builder = new StringBuilder();
+        for (Character nucleotide: dnaStrand.toCharArray()) {
+            builder.append(transcriptions.get(nucleotide));
+        }
+        return builder.toString();
     }
 
 }
